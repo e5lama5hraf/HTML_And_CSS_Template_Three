9fface7cb8154aeee846ba896732ba034233a686
e5lama5hraf <eslamashrafs977@gmail.com>
2026-04-14 21:35:31 +0200

    update

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 let skillsSection = document.querySelector(".our-skills")
 let spans = document.querySelectorAll(".our-skills .progress span");
-// console.log(spans);
 
 window.onscroll = function () {
     if (window.scrollY >= skillsSection.offsetTop - 300) {
@@ -9,3 +8,20 @@ window.onscroll = function () {
         });
     }
 }
+
+let endDate = new Date("Dec 31 2026 23:59:59");
+
+let counter = setInterval(() => {
+    let startDate = new Date();
+    let diffDate = endDate - startDate;
+
+    let days = Math.floor(diffDate / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((diffDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((diffDate % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((diffDate % (1000 * 60)) / (1000));
+
+    document.querySelector(".events .time .days").innerHTML = days < 10 ? `0${days}`: days;
+    document.querySelector(".events .time .hours").innerHTML = hours < 10 ? `0${hours}`: hours;
+    document.querySelector(".events .time .minutes").innerHTML = minutes < 10 ? `0${minutes}`: minutes;
+    document.querySelector(".events .time .seconds").innerHTML = seconds < 10 ? `0${seconds}`: seconds;
+}, 1000);
