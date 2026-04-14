9fa7ac1f59b976a1c84895df3376578f1a25b0c7
e5lama5hraf <eslamashrafs977@gmail.com>
2026-04-14 22:34:11 +0200

    modified

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ window.onscroll = function () {
 }
 
 let endDate = new Date("Dec 31 2026 23:59:59");
-
 let counter = setInterval(() => {
     let startDate = new Date();
     let diffDate = endDate - startDate;
@@ -25,3 +24,27 @@ let counter = setInterval(() => {
     document.querySelector(".events .time .minutes").innerHTML = minutes < 10 ? `0${minutes}`: minutes;
     document.querySelector(".events .time .seconds").innerHTML = seconds < 10 ? `0${seconds}`: seconds;
 }, 1000);
+
+
+let statsSection = document.querySelector(".stats");
+let analysisStats = document.querySelectorAll(".stats .content span");
+let started = false;
+
+window.onscroll = function () {
+    if (window.scrollY >= statsSection - 200) {
+        if (!started) {
+            analysisStats.forEach((element) => increaseNums(element));
+        }
+        started = true;
+    }
+};
+
+function increaseNums(el) {
+    let goal = el.dataset.goal;
+    let counter = setInterval(() => {
+        el.textContent++;
+        if (el.textContent == goal) {
+            clearInterval(counter);
+        }
+    }, 3000 / goal);
+};
